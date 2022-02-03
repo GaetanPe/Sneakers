3f8edf001a47332c4b980422292fc3d3e0dd0377
elimf <elim.florvil@icloud.com>
2022-02-03 11:20:37 +0100

    retour

--- project-sneakers/src/App.vue
+++ project-sneakers/src/App.vue
@@ -1,31 +1,76 @@
 <template>
-
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <div
+        class="container-fluid navbar navbar-expand-lg navbar-light bg-light"
+        id="nav"
+    >
+        <div class="row">
+            <div class="col-5 ">
+                <img
+                    src="./assets/img/nav-barLogo.png"
+                    alt=""
+                    style="height: 100px"
+                />
+            </div>
+            <div class="col-4 my-auto p-2" style="margin-left:100px">
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div
+                    class="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+                >
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <router-link to="/">Home</router-link>  
+                        </li>
+                        <li class="nav-item">
+                            <router-link to="/about">About</router-link>  
+                        </li>
+                        <li class="nav-item">
+                            <router-link to="/store">Store</router-link>  
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <router-view />
 </template>
-
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    padding: 30px;
+    z-index: 3;
+    margin-bottom: 300px;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+    font-weight: bold;
+    color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
-  color: #b9b142;
+    color:yellow;
+}
+li{
+  text-decoration: none;
 }
 </style>
--- project-sneakers/src/components/HelloWorld.vue
+++ project-sneakers/src/components/HelloWorld.vue
@@ -1,44 +1,7 @@
 <template>
     <body id="page-top">
         <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-info fixed-top bg-light" id="mainNav">
-            <div class="container">
-                <a class="navbar-brand" href="#page-top"
-                    ><img src="../assets/img/nav-barLogo.png" alt="..." style="height:100px"
-                /></a>
-                <button
-                    class="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarResponsive"
-                    aria-controls="navbarResponsive"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    Menu
-                    <i class="fas fa-bars ms-1"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#services">Services</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#portfolio">Portfolio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#about">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#team">Team</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#contact">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        
         <!-- Masthead-->
         <header class="masthead">
             <div class="container">
